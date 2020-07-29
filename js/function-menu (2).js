02c7e331e87a521f73df2d8fb26045e16a3724f4
sarahhonorio <sarah.martins01@etec.sp.gov.br>
2020-07-29 12:39:31 -0300

    Commit - categoria

--- js/function-menu (2).js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $('.categoria').click(function(e) {
-        e.preventDefault()
-        $('#conteudo').empty()
-        $('#conteudo').load('src/categorias/visao/list-categoria.html')
-    })
-})
