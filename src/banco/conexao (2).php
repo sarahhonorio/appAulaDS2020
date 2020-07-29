27c83e3b00dae74d0b49337df1d5b99300d4aa02
sarahhonorio <sarah.martins01@etec.sp.gov.br>
2020-07-29 20:00:05 -0300

    Commit - categoria

--- src/banco/conexao (2).php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include('config.php');
-
-$conexao = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
--- src/banco/config (2).php
+++
@@ -1,8 +0,0 @@
-<?php
-
-date_default_timezone_set('America/Sao_Paulo');
-
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASSWORD', '');
-define('DBNAME', 'dbaulads');
