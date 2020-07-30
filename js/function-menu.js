b4fa4e4d51d0b6856e79524a2d624fe29be9621c
sarahhonorio <sarah.martins01@etec.sp.gov.br>
2020-07-30 12:42:17 -0300

    Commit - function

--- js/function-menu.js
+++ js/function-menu.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
         e.preventDefault()
         $('#conteudo').empty()
         $('#conteudo').load('src/categorias/visao/list-categoria.html')
+    })
+    $('.cliente').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
         $('#conteudo').load('src/clientes/visao/list-cliente.html')
     })
 })
